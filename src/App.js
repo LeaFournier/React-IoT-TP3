21a749837f75c56a6af5340e39ed234181b70c7e
leaFournier <lea.fournier@etud.univ-jfc.fr>
2021-12-10 12:03:18 +0100

    Rechargement automatique bpm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Instant from './component/instantHeartrate';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const [measurement, setMeasurement] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setMeasurement(Instant);
+    }, 2000);
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
+      <h1>
+          Heartrate Monitoring
+      </h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Instant />  
       </header>
     </div>
   );
+
+
 }
 
 export default App;
