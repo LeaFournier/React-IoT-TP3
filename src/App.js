7547aa1542887cca26fe052214290d91e348a3f6
leaFournier <lea.fournier@etud.univ-jfc.fr>
2021-12-10 14:29:50 +0100

    Rechargement automatique bpm + couleur

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-import Instant from './component/instantHeartrate';
+import InstantHeartrate from './component/instantHeartrate';
 import { useEffect, useState } from 'react';
 
+function randomBpm() {
+  return Math.floor(Math.random() * 160) + 40;
+}
 
 function App() {
-  const [measurement, setMeasurement] = useState(0);
+  const [measurement, setMeasurement] = useState([50]);
 
   useEffect(() => {
     setInterval(() => {
-      setMeasurement(Instant);
-    }, 2000);
-  }, []);
+    const bpm = { timestamp: new Date().getTime(),
+      hearthrate: randomBpm(),
+    };
+    setMeasurement((prev) => [...prev, bpm]);
+  }, 5000)
+}, []) 
+     
+    const bpm = measurement[measurement.length - 1 ];
+
+    console.log(measurement);
+
+    console.log(bpm.timestamp);
+ 
   return (
     <div className="App">
       <header className="App-header">
@@ -19,7 +32,7 @@ function App() {
           Heartrate Monitoring
       </h1>
         <img src={logo} className="App-logo" alt="logo" />
-      <Instant />  
+        <InstantHeartrate data={bpm.hearthrate} />
       </header>
     </div>
   );
